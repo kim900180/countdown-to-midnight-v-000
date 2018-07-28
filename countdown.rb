2492f0dfe4862c0c5764b41e53bcde93c555dd44
Jae Yong Kim <kim900180@gmail.com>
2018-07-28 00:10:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  counter = 0
-  while counter < 10
+  counter = number
+  while counter > 0
     "#{number} SECOND(S)!"
-    counter += 1
+    counter -= 1
   end
   return "HAPPY NEW YEAR!"
 end
